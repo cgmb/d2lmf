a6015e93778661c5f0dd14edee3c4f86b7b135cc
Cory Bloor <cgbloor@ucalgary.ca>
2015-10-03 03:29:48 -0600

    Add rename function to merge submissions

--- d2lmf/d2lmf.py
+++ d2lmf/d2lmf.py
@@ -10,11 +10,74 @@ import errno
 __version__ = "0.0.1"
 
 def makedirs_exist(path):
+    """
+    Makes a directory at the given path without raising an error if it already exists
+    """
     try:
         os.makedirs(path)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
+
+class ParserError(Exception):
+    pass
+
+def parse_submission_dirname(dirname):
+    """
+    Parses a directory name in the form '<id_number> - <student_name> - <timestamp>'
+    """
+    seperator = ' - '
+    tokens = dirname.split(seperator)
+    if len(tokens) < 3:
+        raise ParserError('Expected hyphen-separated id, name and timestamp'
+                ' in "%s"' % dirname)
+    id_number = tokens[0]
+    # we'll assume the extra hypens are a part of the student's name
+    student_name = seperator.join(tokens[1:-1])
+    timestamp = tokens[-1]
+    return (id_number, student_name, timestamp)
+
+def merge(src, dest):
+    """
+    Merges the src folder into the dest folder
+    """
+    import shutil
+    for src_root, dirs, files in os.walk(src):
+        dest_root = src_root.replace(src, dest, 1)
+        if not os.path.exists(dest_root):
+            os.makedirs(dest_root)
+        for f in files:
+            src_file = os.path.join(src_root, f)
+            dest_file = os.path.join(dest_root, f)
+            if os.path.exists(dest_file):
+                os.remove(dest_file)
+            shutil.move(src_file, dest_root)
+    shutil.rmtree(src)
+
+def rename(args):
+    from operator import itemgetter
+    from datetime import datetime
+    submissions = []
+    for name in os.listdir(args.input_folder):
+        if os.path.isdir(os.path.join(args.input_folder, name)):
+            try:
+                id_num, student, timestamp = parse_submission_dirname(name)
+                parsed_timestamp = datetime.strptime(timestamp,
+                        '%b %d, %Y %I:%M %p') # Sep 29, 2015 4:17 PM
+                shortname = student.replace(' ', '')
+                submissions.append((name, shortname, parsed_timestamp))
+            except (ParserError,ValueError) as e:
+                print(e)
+                pass
+    # sort by student, then by date
+    sorted(submissions, key=itemgetter(1,2))
+    for dirname, student_name, timestamp in submissions:
+        oldpath = os.path.join(args.input_folder, dirname)
+        newpath = os.path.join(args.input_folder, student_name)
+        if os.path.exists(newpath):
+            merge(oldpath, newpath)
+        else:
+            os.rename(oldpath, newpath)
 
 def extract(args):
     import zipfile
@@ -34,6 +97,11 @@ def main():
     extract_parser.add_argument('output_folder',
             help='The folder in which to put extracted data.')
     extract_parser.set_defaults(func=extract)
+
+    rename_parser = subparsers.add_parser('rename', help='')
+    rename_parser.add_argument('input_folder',
+            help='The folder full of student submissions to rename.')
+    rename_parser.set_defaults(func=rename)
 
     args = parser.parse_args()
     args.func(args)

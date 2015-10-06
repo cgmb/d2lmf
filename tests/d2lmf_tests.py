aadf7421cc398185af7f1af5506c5f74f68ac761
Cory Bloor <cgbloor@ucalgary.ca>
2015-10-06 17:38:55 -0600

    Add first real tests
    Unfortunately, these don't work yet. I need to investigate how to
    import the functions I want from the d2lmf module into my tests
    without making these things public library functions.

--- tests/d2lmf_tests.py
+++ tests/d2lmf_tests.py
@@ -1,11 +1,16 @@
 from nose.tools import *
 import d2lmf
 
-def setup():
-    print "SETUP!"
+def test_parse_submission_dirname():
+    student_id, student_name, submission_date = d2lmf.parse_submission_dirname(
+            '840137-90210 - Johnny Cashe - Sep 12, 2015 6:31 PM')
+    assert student_id == '840137-90210'
+    assert student_name == 'Jonny Cache'
+    assert submission_date == 'Sep 12, 2015 6:31 PM'
 
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
-    print "I RAN!"
+def test_parse_submission_dirname_with_hyphen_in_name():
+    student_id, student_name, submission_date = d2lmf.parse_submission_dirname(
+            '928421-33831 - Mary-Kate Olsen - Oct 2, 2015 12:22 AM')
+    assert student_id == '928421-33831'
+    assert student_name == 'Mary-Kate Olsen'
+    assert submission_date == 'Oct 2, 2015 12:22 AM'

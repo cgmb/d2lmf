8e477bfaf706cfd4bd1f30b3d4538850bb232b62
Cory Bloor <cgbloor@ucalgary.ca>
2015-10-14 17:17:48 -0600

    Fix initial tests

--- tests/d2lmf_tests.py
+++ tests/d2lmf_tests.py
@@ -1,15 +1,15 @@
 from nose.tools import *
-import d2lmf
+from d2lmf.d2lmf import parse_submission_dirname
 
 def test_parse_submission_dirname():
-    student_id, student_name, submission_date = d2lmf.parse_submission_dirname(
-            '840137-90210 - Johnny Cashe - Sep 12, 2015 6:31 PM')
+    student_id, student_name, submission_date = parse_submission_dirname(
+            '840137-90210 - Johnny Cache - Sep 12, 2015 6:31 PM')
     assert student_id == '840137-90210'
-    assert student_name == 'Jonny Cache'
+    assert student_name == 'Johnny Cache'
     assert submission_date == 'Sep 12, 2015 6:31 PM'
 
 def test_parse_submission_dirname_with_hyphen_in_name():
-    student_id, student_name, submission_date = d2lmf.parse_submission_dirname(
+    student_id, student_name, submission_date = parse_submission_dirname(
             '928421-33831 - Mary-Kate Olsen - Oct 2, 2015 12:22 AM')
     assert student_id == '928421-33831'
     assert student_name == 'Mary-Kate Olsen'

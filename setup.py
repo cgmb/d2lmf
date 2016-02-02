daac8765831ffaf19c99117fe6fccaee8980b14d
Cory Bloor <cgbloor@ucalgary.ca>
2016-02-02 15:58:01 -0700

    Copy README into setup.py's long_description
    This provides the description on the PyPI project page, which is the
    first thing that comes up in a Google search for 'd2lmf'.

--- setup.py
+++ setup.py
@@ -38,6 +38,71 @@ setup(
          'Topic :: Education',
     ],
     long_description = """
-d2lmf is a command-line tool to help mark assignments submitted to D2L (Desire2Learn).
+How to Use
+----------
+
+A sample command might be:
+
+.. code:: bash
+
+    d2lmf extract -xjcm "Assignment 1 Download Oct 11, 2015 803 PM.zip" mark/a1/T01
+
+This extracts the submissions from the zip folder into the directory
+``mark/a1/T01``. The flags x, j, c and m specify that we want to extract
+any zip, rar, tar, or 7z archives that students submitted, delete 'junk'
+like ``.DS_Store``, collapse needlessly nested directories, and merge
+all submitted files into a directory for each student.
+
+On Windows, you may need to invoke d2lmf through Python, like so:
+
+.. code:: bash
+
+    python -m d2lmf extract -xjcm "Assignment 1 Download Oct 11, 2015 803 PM.zip" mark/a1/T01
+
+How to Install
+--------------
+
+Ubuntu
+~~~~~~
+
+.. code:: bash
+
+    sudo apt-get install python-pip
+    sudo pip install d2lmf
+
+OSX
+~~~
+
+.. code:: bash
+
+    curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+    sudo python get-pip.py
+    sudo pip install d2lmf
+
+Windows
+~~~~~~~
+
+First, download and install Python 2.7 from
+https://www.python.org/downloads/
+
+Then, install d2lmf with the command below:
+
+.. code:: bash
+
+    python -m pip install d2lmf
+
+Dependencies
+------------
+
+The extraction of rar and 7z archives depends on external tools. If you
+do not have those tools installed, those archives will be skipped.
+
+On Windows, only zip files may be extracted.
+
+Release Status
+--------------
+
+Note the version number: ``0.0.3``. There may be bugs, and the interface
+may undergo significant changes between releases.
 """
 )
